40127bd09edafa8fc20e46daa0e236af456c2028
xin7c <xin7c@qq.com>
2017-03-07 17:13:43 +0800

    备注：    //获取浏览器宽度

--- static/local.js
+++ static/local.js
@@ -3,7 +3,8 @@
  * @author: xuchu(xuchu@acfun.tv) on 17/3/6.
  */
 $(document).ready(function(){
-    var body_width = document.body.clientWidth;
+    //获取浏览器宽度
+    var body_width = $(document.body).width();
     var h_th_1 = $('#table').offset().top;
     var w_table = $("table").find("tr").width() + 30;
     $('#ssh_front_result').offset({ top: h_th_1});
@@ -11,6 +12,5 @@ $(document).ready(function(){
     $('#ssh_front_result').css({
         "width":body_width-w_table
     });
-    console.log(body_width-w_table)
 
 });//JS end
