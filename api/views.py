73344373456e77284656e2e6cd002da6ebfebfc3
xin7c <xin7c@qq.com>
2017-03-10 15:16:15 +0800

    add api/r 添加模板判断,默认选中逻辑

--- api/views.py
+++ api/views.py
@@ -5,10 +5,14 @@ from .models import API_VERSION
 def api(req):
     if req.method == "GET":
         addr = req.META['REMOTE_ADDR']
-        apis = API_VERSION.objects.all()
+        apis = API_VERSION.objects.values()
+        apis_list = [x["api_version"] for x in apis]
+        last_version = max(apis_list)
         context = {}
         context['addr'] = addr
         context['apis'] = apis
+        context['apis_list'] = apis_list
+        context['last_version'] = last_version
         response = render(req, 'api.html', context=context)
         response["hd"] = "1024"
         response.set_cookie(key="cookie_api", value="2017")
